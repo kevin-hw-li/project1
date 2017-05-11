ef3b65b6a10f94b33ede31de7897d982f06e624b
kevin-hw-li <hoi.li@unswalumni.com>
2017-05-11 17:45:53 +1000

    some changes

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,5 +1,7 @@
 class Friendship < ApplicationRecord
   validates :friend_id, uniqueness: { scope: :user_id }
+  #validates each friend_id and the user_id in the friendship is unique
+  #this avoids adding the same user as friend multiple times
   belongs_to :user
   belongs_to :friend, :class_name => "User"
 end
